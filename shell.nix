69b36a4ffebb8fb048eb373257f9acf0ba69c982
cathe <pearsandcabbage@gmail.com>
2026-04-13 22:08:49 +0100

    faffing with nix.shell

--- shell.nix
+++ shell.nix
@@ -1,8 +1,34 @@
-{ pkgs ? import <nixpkgs> { }}:
+{
+  pkgs ? import <nixpkgs> { },
+    # overlays = [
+    #   (final: prev: {
+    #     zshconfig = prev.callPackage /home/cathe/dots/.config/home-manager/zsh { };
+    #   })
+    # ];
+  # },
+  # lib
+}:
 
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    zsh
-    opam
-  ];
-}
+# pkgs.callPackage (
+#   {
+#     mkShell,
+#     zshconfig,
+#   }:
+let zshconfig = import /home/cathe/dots/.config/home-manager/zsh { inherit pkgs; }; 
+#     zshconfig = zshconfig_g.overrideDerivation (oldAttrs: {
+#     programs.zsh.oh-my-zsh.theme = "custom";
+# });
+    
+
+in
+  pkgs.mkShell {
+    # strictDeps = true;
+    inputsFrom = [
+      zshconfig
+    ];
+    buildInputs = with pkgs; [
+      zsh
+      opam
+    ];
+  }
+# ) { }
