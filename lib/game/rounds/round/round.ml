8ff00f684677b538c2cfd53aa8462481e67f5c7a
cathe <pearsandcabbage@gmail.com>
2026-04-20 17:25:41 +0100

    delete old code

--- lib/game/rounds/round/round.ml
+++ lib/game/rounds/round/round.ml
@@ -19,55 +19,6 @@ let get_random_player (x : Roles.Role.Alignment.t) : Players.t -> Player.t =
   Players.random ~f:(Players.aligned x)
 ;;
 
-(* let get_ability (x : Player.t) : Abilities.t = Abilities.make x.role *)
-(* let map_player_ability x acc : unit = get_ability x |> Player.Map.replace acc x *)
-
-(* let rec map_players_ability acc : Player.t list -> unit = function
-   | [] ->
-   print_endline "exit map_players_ability";
-   ()
-   | h :: tl ->
-   Printf.printf "mapping: %s\n" (Player.show h);
-   map_player_ability h acc;
-   Printf.printf "cont. mapping: %s\n" (Players.show (Players.of_list tl));
-   map_players_ability acc tl
-   ;;
-
-   let rec populate_ability_map
-   (xs : Players.t)
-   ?(acc : Abilities.t Player.Map.t = Player.Map.create (Players.cardinal xs))
-   (rm : bool Role.Map.t)
-   : Abilities.t Player.Map.t
-   =
-   (fun () -> Players.to_list xs |> map_players_ability acc)
-   |> handle_populate_ability_map xs acc rm;
-   acc
-
-   and handle_populate_ability_map xs acc rm (f : unit -> unit) : unit =
-   match f () with
-   | () -> ()
-   (* | effect e, _ ->
-   Printf.printf
-   "Unhandled effect: %s\n"
-   (Obj.Extension_constructor.name (Obj.Extension_constructor.of_val e));
-   raise (Effect.Unhandled e) *)
-   (* TODO: effects... *)
-   | effect Abilities.Abilities.GetTargetPlayer, k ->
-   let x = get_random_player Role.Good xs in
-   Printf.printf "target player: %s\n" (Player.show x);
-   handle_populate_ability_map xs acc rm (fun () ->
-   Effect.Deep.continue k x.index)
-   | effect Abilities.Abilities.AddExtraOutsiders (n : int), k ->
-   let replaced = Players.replace_n_kinds n Townsfolk Outsider rm xs in
-   Printf.printf "replaced: %s\n" (Players.show replaced);
-   let need_updating = Player.Map.to_seq_keys acc |> Players.of_seq in
-   Printf.printf "need updating: %s\n" (Players.show need_updating);
-   let ys = Players.inter replaced need_updating in
-   handle_populate_ability_map ys acc rm (fun () ->
-   Players.to_list ys |> map_players_ability acc);
-   handle_populate_ability_map xs acc rm (fun () -> Effect.Deep.continue k ())
-   ;; *)
-
 let initial
       ?(starting : Phase.t = Phase.Day)
       (rolemap : bool Roles.Map.t)
