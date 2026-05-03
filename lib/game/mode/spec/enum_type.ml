370e48394370c20d678c8e8fcc7f26efc05a54ba
cathe <pearsandcabbage@gmail.com>
2026-05-03 13:33:49 +0100

    incr/decr amount

--- lib/game/mode/spec/enum_type.ml
+++ lib/game/mode/spec/enum_type.ml
@@ -10,7 +10,8 @@ module type S = sig
   exception EnumOutOfBounds of int
 
   val of_int : int -> t
-
+  val next : ?wrap:bool -> ?amount:int -> t -> t
+  val prev : ?wrap:bool -> ?amount:int -> t -> t
   val collect : unit -> t list
 end
 
@@ -25,6 +26,20 @@ module Make (X : InputS) : S with type t = X.t = struct
 
   let of_int (n : int) : t =
     match of_enum n with Some x -> x | None -> raise (EnumOutOfBounds n)
+  ;;
+
+  let next ?(wrap : bool = true) ?(amount : int = 1) (x : t) : t =
+    let n : int = to_enum x in
+    if Int.equal n max
+    then if wrap then raise (EnumOutOfBounds n) else of_int 0
+    else of_int (n + amount)
+  ;;
+
+  let prev ?(wrap : bool = true) ?(amount : int = 1) (x : t) : t =
+    let n : int = to_enum x in
+    if Int.equal n min
+    then if wrap then raise (EnumOutOfBounds n) else of_int max
+    else of_int (n - amount)
   ;;
 
   let collect () : t list = List.init (max + 1) of_int
