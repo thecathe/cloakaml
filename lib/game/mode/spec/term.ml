82b61aca132af6d4a79f3eb31c8e8b3c872c32d5
cathe <pearsandcabbage@gmail.com>
2026-05-07 20:28:41 +0100

    term spec

---
+++ lib/game/mode/spec/term.ml
@@ -0,0 +1,17 @@
+(** @canonical Cloakaml.Game.Mode.Spec.Term *)
+
+(** {1 unused} *)
+
+exception TermTodo
+
+module type S = sig
+  type t [@@deriving show { with_path = false }, eq]
+end
+
+module type InputS = sig
+  type t [@@deriving show { with_path = false }, eq]
+end
+
+module Make (X : InputS) : S with type t = X.t = struct
+  include X
+end
