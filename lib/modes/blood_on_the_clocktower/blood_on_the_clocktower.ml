3d26df19552879b10e39f93e13e8f10b6c8cc9ae
cathe <pearsandcabbage@gmail.com>
2026-05-08 16:11:10 +0100

    sketching out events

--- lib/modes/blood_on_the_clocktower/blood_on_the_clocktower.ml
+++ lib/modes/blood_on_the_clocktower/blood_on_the_clocktower.ml
@@ -23,19 +23,85 @@ include Build ()
 type phase = Rounds.Round.Data.phase
 type status = Players.Player.status
 
+module Event = struct
+  type 'a transition =
+    { before : 'a
+    ; after : 'a
+    }
+
+  let transition (before : 'a) (after : 'a) : 'a transition = { before; after }
+
+  type phase =
+    | Day
+    | Night
+
+  type status =
+    | Alive
+    | Dead
+    | Alive_And of extra
+  [@@deriving show { with_path = false }, eq]
+
+  and extra =
+    | Asleep
+    | Nominated
+    | Poisoned
+    | Both of (extra * extra)
+
+  type change =
+    | Phase of phase transition
+    | Status of status transition
+
+  (* *)
+  let phase_transition : phase -> phase -> phase transition = transition
+
+  (* *)
+  let day_to_night : phase transition = phase_transition Day Night
+  let night_to_day : phase transition = phase_transition Night Day
+
+  (* *)
+  let phase_change (x : phase transition) : change = Phase x
+  let end_of_day : change = phase_change day_to_night
+  let end_of_night : change = phase_change night_to_day
+
+  (* *)
+  let start_of_day : change = end_of_night
+  let start_of_night : change = end_of_day
+
+  (* *)
+  let status_transition : status -> status -> status transition = transition
+
+  (* *)
+  let died : status transition = status_transition Alive Dead
+  let is_nominated : status = Alive_And Nominated
+  let nominated : status transition = status_transition Alive is_nominated
+
+  (* *)
+  let status_change (x : status transition) : change = Status x
+  let death : change = status_change died
+  let nomination : change = status_change nominated
+
+  (** ew ugly -- this is what we would have to do without the above *)
+  let nomination : change =
+    Status { before = Alive; after = Alive_And Nominated }
+  ;;
+end
+
 (* type ability = { trigger : trigger; condition: } *)
 type ability = { is_enabled : enabled_when }
 
 and enabled_when =
   | Always (** Always enabled *)
   | If of condition (** If condition holds *)
+  | Iff of event_sequence (** If-and-only-if sequence of events holds *)
   | Triggered of event (** Triggered by event *)
+
+and event_sequence =
+  | First of event_sequence
+  | Next of event * event_sequence
+  | Finally of event
 
 and condition =
   | Query of query (** Query the state *)
-  | On of event (** At the beginning of the event *)
-  | After of event (** At the end of the event *)
-  | Is of phase (** Is the phase *)
   | During of scope * event * query (** If query holds in specific event *)
   | And of condition * condition (** Both conditions *)
   | Or of condition * condition
@@ -43,9 +109,23 @@ and condition =
   | All of condition list
 
 and event =
-  | Phase of phase
-  | Status of status
+  | On of event (** At the beginning of the event *)
+  | After of event (** At the end of the event *)
+  | Is of phase (** Is the phase *)
   | Status_At of status * event
+
+and event_change =
+  | Phase of phase event_transition
+  | Status of status event_transition
+
+and 'a event_transition =
+  { before : 'a
+  ; after : 'a
+  }
+
+and event_frame =
+  | Phase of phase
+  | Status_Change of status
 
 and scope = Previous
 
@@ -73,7 +153,7 @@ and query_op =
 
 (** {2 Infixes} *)
 
-let death = Status Dead
+(* let death = Status Dead
 let on_death = On death
 
 (* *)
@@ -108,13 +188,20 @@ let only_3_players_alive : query = num Players == 3
 let no_execution_occurred : condition = yesterday (Not (Any Nomination))
 
 let mayor_1 : enabled_when =
-  If (All [ end_of_day
-          ; there_are only_3_players_alive
-          ; no_execution_occurred
-          ])
+  If (All [ end_of_day; there_are only_3_players_alive; no_execution_occurred ])
+;;
+
+let mayor_1 : enabled_when =
+  Iff
+    (First
+       (Next
+          ( end_of_day
+          , Next
+              (no_execution_occurred, Finally (there_are only_3_players_alive))
+          )))
 ;;
 
 (** {4 Part 2: If you {b die} at night, another player {i might} {b die} instead.}
 *)
 
-let mayor_2 = Triggered (Status_At (Dead, night))
+let mayor_2 = Triggered (Status_At (Dead, night)) *)
