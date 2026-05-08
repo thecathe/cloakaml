f7d4c1222f4b0c61cbef2ec329c18eedc97c7f45
cathe <pearsandcabbage@gmail.com>
2026-05-08 09:35:33 +0100

    removed local zsh config input

--- shell.nix
+++ shell.nix
@@ -1,40 +1,23 @@
 {
   pkgs ? import <nixpkgs> { },
-    # overlays = [
-    #   (final: prev: {
-    #     zshconfig = prev.callPackage /home/cathe/dots/.config/home-manager/zsh { };
-    #   })
-    # ];
-  # },
-  # lib
 }:
 
-# pkgs.callPackage (
-#   {
-#     mkShell,
-#     zshconfig,
-#   }:
-let zshconfig = import /home/cathe/dots/.config/home-manager/zsh { inherit pkgs; };
-#     zshconfig = zshconfig_g.overrideDerivation (oldAttrs: {
-#     programs.zsh.oh-my-zsh.theme = "custom";
-# });
-
-
-in
-  pkgs.mkShell {
-    # strictDeps = true;
-    inputsFrom = [
-      zshconfig
-    ];
-    buildInputs = with pkgs; [
-      zsh
-      opam
-      erlang
-      rebar3
-    ];
-    shellHook = ''
-      eval $(opam env)
-      opam switch cloakaml
-    '';
-  }
-# ) { }
+# let
+#   zshconfig = import /home/cathe/dots/.config/home-manager/zsh { inherit pkgs; };
+# in
+pkgs.mkShell {
+  # strictDeps = true;
+  # inputsFrom = [
+  #   zshconfig
+  # ];
+  buildInputs = with pkgs; [
+    zsh
+    opam
+    erlang
+    rebar3
+  ];
+  shellHook = ''
+    eval $(opam env)
+    opam switch cloakaml
+  '';
+}
